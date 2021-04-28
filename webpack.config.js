41a0b4c6a2b656729d5fc61a21a90fccf8cd6f9d
ARCHER04 <62651374+ARCHER04@users.noreply.github.com>
2021-04-28 19:22:52 +0300

    les-1 1.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+
 const path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
    entry: {
-       app: './index.js',
+       app: './index.jsx',
    },
    context: path.resolve(__dirname, "static_src"),
    output: {
        path: path.resolve(__dirname, "static", "build"),
        filename: 'app.js',
+       publicPath: '/static/build/',
+   },
+
+   module: {
+       rules: [
+           {
+               test: /\.(js|jsx)$/,
+               include: path.resolve(__dirname, "static_src"),
+               loader: 'babel-loader',
+               exclude: /node_modules/,
+               options: {
+                 presets: ['@babel/env', '@babel/react'],
+               }
+           },
+       ],
    },
 };
+
+
 /*module.exports = {
     ...
     module: {
@@ -29,4 +47,21 @@ module.exports = {
     
     ...
     }
-    */
+
+module.exports = {
+    ...
+    
+    module: {
+           rules: [
+               {
+                   ...
+                   query: {
+                     presets: ['@babel/env', '@babel/react'],
+                   }
+               },
+           ],
+       },
+    
+    ...
+    }
+  */
